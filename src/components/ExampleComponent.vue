d81f092b2720797ea1e079a371d329ffd3e3d46f
muthukumaran-muthiah <120544258+muthukumaran-muthiah@users.noreply.github.com>
2024-04-24 14:00:18 +0530

    integrated chat api

--- src/components/ExampleComponent.vue
+++ src/components/ExampleComponent.vue
@@ -8,18 +8,18 @@
           avatar="https://cdn.quasar.dev/img/avatar4.jpg"
           :text="[message.text]"
           sent
-          stamp="7 minutes ago"
+          :stamp="message.key"
         />
         <q-chat-message
           v-else
           name="Leo"
           avatar="https://cdn.quasar.dev/img/avatar3.jpg"
           :text="[message.text]"
-          stamp="4 minutes ago"
+          :stamp="message.key"
         />
       </template>
     </div>
-    <div class="q-pa-md" style="width: 100%; display: flex; gap: 12px;">
+    <div class="q-pa-md" style="width: 100%; display: flex; gap: 12px">
       <q-input
         @keydown.enter.prevent="submit"
         style="width: 90%"
@@ -28,13 +28,15 @@
         autogrow
       />
       <span class="chatButton">
-      <q-icon style="widows: 10%;" name="send" />
+        <q-icon @click="submit" style="widows: 10%" name="send" />
       </span>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   // Properties returned from data() become reactive state
   // and will be exposed on `this`.
@@ -50,15 +52,47 @@ export default {
   methods: {
     submit() {
       this.messages.push({
-        key: Date(),
+        key: new Date().toLocaleTimeString(),
         sender: 'self',
         text: this.text,
       });
 
-      this.messages.push({
-        key: Date(),
-        text: this.text,
-      });
+      let config = {
+        headers: {
+          Authorization: 'Bearer ' + process.env.OPENAI_KEY,
+        },
+      };
+      let bodyParameters = {
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'user',
+            content: this.text,
+          },
+        ],
+        temperature: 1,
+        top_p: 1,
+        n: 1,
+        stream: false,
+        max_tokens: 250,
+        presence_penalty: 0,
+        frequency_penalty: 0,
+      };
+      axios
+        .post(
+          'https://api.openai.com/v1/chat/completions',
+          bodyParameters,
+          config
+        )
+        .then((response) => {
+          console.log(response);
+          console.log(response.data.choices);
+          this.messages.push({
+            key: new Date().toLocaleTimeString(),
+            text: response.data.choices[0].message.content,
+            sender: response.data.choices[0].message.role,
+          });
+        });
 
       this.text = null;
     },
