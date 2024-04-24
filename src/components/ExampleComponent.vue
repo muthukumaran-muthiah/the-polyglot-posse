205050716dca4ad2ca0ac6f4fe101a65f9fd3465
muthukumaran-muthiah <120544258+muthukumaran-muthiah@users.noreply.github.com>
2024-04-24 11:06:01 +0530

    chat added.

--- src/components/ExampleComponent.vue
+++ src/components/ExampleComponent.vue
@@ -1,37 +1,43 @@
 <template>
-  <div>
-    <p>{{ title }}</p>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id" @click="increment">
-        {{ todo.id }} - {{ todo.content }}
-      </li>
-    </ul>
-    <p>Count: {{ todoCount }} / {{ meta.totalCount }}</p>
-    <p>Active: {{ active ? 'yes' : 'no' }}</p>
-    <p>Clicks on todos: {{ clickCount }}</p>
+  <div class="q-pa-md row justify-center">
+    <div style="width: 100%; max-width: 400px">
+      <q-chat-message
+        name="me"
+        avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+        :text="['hey, how are you?']"
+        sent
+        stamp="7 minutes ago"
+      />
+      <q-chat-message
+        name="Jane"
+        avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+        :text="[`doing fine, how r you?`]"
+        stamp="4 minutes ago"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import { Todo, Meta } from './models';
+// import { computed, ref } from 'vue';
+// import { Todo, Meta } from './models';
 
-interface Props {
-  title: string;
-  todos?: Todo[];
-  meta: Meta;
-  active: boolean;
-};
+// interface Props {
+//   title: string;
+//   todos?: Todo[];
+//   meta: Meta;
+//   active: boolean;
+// }
 
-const props = withDefaults(defineProps<Props>(), {
-  todos: () => []
-});
+// const props = withDefaults(defineProps<Props>(), {
+//   todos: () => [],
+// });
 
-const clickCount = ref(0);
-function increment() {
-  clickCount.value += 1;
-  return clickCount.value;
-}
+// const clickCount = ref(0);
+// function increment() {
+//   clickCount.value += 1;
+//   return clickCount.value;
+// }
 
-const todoCount = computed(() => props.todos.length);
+// const todoCount = computed(() => props.todos.length);
 </script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,10 +12,10 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Leo chat
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>Leo v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
@@ -28,7 +28,7 @@
         <q-item-label
           header
         >
-          Essential Links
+          Leo speaks
         </q-item-label>
 
         <EssentialLink
@@ -55,46 +55,16 @@ defineOptions({
 
 const linksList: EssentialLinkProps[] = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
+    title: 'Chat',
+    caption: 'leo.chat',
+    icon: 'chat',
     link: 'https://quasar.dev'
   },
   {
     title: 'Github',
-    caption: 'github.com/quasarframework',
+    caption: '/muthukumaran-muthiah/the-polyglot-posse',
     icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+    link: 'https://github.com/muthukumaran-muthiah/the-polyglot-posse'
   }
 ];
 
